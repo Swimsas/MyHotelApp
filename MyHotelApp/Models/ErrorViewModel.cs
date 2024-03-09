e36b4dac7a9d250d36f444deabef753f0a61d319
Ivan Ivanov <swimsas@gmail.com>
2024-03-09 11:44:17 +0200

    Add project files.

---
+++ MyHotelApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyHotelApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
