5ec4e55491b3dceb96d08ab3bd8500cbe2321d7e
Ivan Ivanov <swimsas@gmail.com>
2024-03-09 14:46:10 +0200

    Add layers to project (Core , Infrastructure)

--- MyHotelApp/Extensions/ServiceCollectionExtension.cs
+++ MyHotelApp/Extensions/ServiceCollectionExtension.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using MyHotelApp.Data;
+using MyHotelApp.Infrastructure.Data;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
