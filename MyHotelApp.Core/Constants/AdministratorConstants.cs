c73b236c368fc6b1daaaaa8dfc52da1c4d726e75
Ivan Ivanov <swimsas@gmail.com>
2024-04-07 13:37:27 +0300

    Extented claims with IsAdmin method

---
+++ MyHotelApp.Core/Constants/AdministratorConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace MyHotelApp.Core.Constants
+{
+    public static class AdministratorConstants
+    {
+        public const string AdminRole = "Admin";
+        public const string AdminAreaName = "Admin";
+    }
+}
--- MyHotelApp/Extensions/ClaimsPrincipalExtensions.cs
+++ MyHotelApp/Extensions/ClaimsPrincipalExtensions.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using static MyHotelApp.Core.Constants.AdministratorConstants;
 
 namespace System.Security.Claims
 {
@@ -7,6 +7,11 @@ namespace System.Security.Claims
         public static string Id(this ClaimsPrincipal user)
         {
             return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+
+        public static bool IsAdmin(this ClaimsPrincipal user)
+        {
+            return user.IsInRole(AdminRole);
         }
     }
 }
